d3c297422a9ee3a578e65f217f5e0c9ecb52fe2e
Shriram Lahane <172600005+shriram-02@users.noreply.github.com>
2026-09-08 07:34:55 +0530

    Add files via upload

---
+++ GFG-POTD-Solutions/Word in Grid - All Occurrences.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> searchWord(vector<vector<char>> &mat, string &word) {
+        int n = mat.size(), m = mat[0].size();
+        vector<vector<int>> ans;
+
+        int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] != word[0]) continue;
+
+                for (int d = 0; d < 8; d++) {
+                    int x = i, y = j;
+                    int k = 0;
+
+                    while (k < (int)word.size()) {
+                        if (x < 0 || x >= n || y < 0 || y >= m ||
+                            mat[x][y] != word[k])
+                            break;
+
+                        x += dx[d];
+                        y += dy[d];
+                        k++;
+                    }
+
+                    if (k == (int)word.size()) {
+                        ans.push_back({i, j});
+                        break;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
